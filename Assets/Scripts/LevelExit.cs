2095e8c6149032d0020e9c8ef25937d54949f509
Cha Seoyeon <seoyeon0833@gmail.com>
2024-02-16 02:20:50 +0900

    레벨 종료 포털 만들기

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    float time = 2f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            StartCoroutine(Exit());
+        }
+    }
+
+    IEnumerator Exit()
+    {
+        yield return new WaitForSecondsRealtime(time);
+
+        int currnetSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currnetSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
